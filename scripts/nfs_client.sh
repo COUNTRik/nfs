4265bf55ad6922aaeff0786230a6ed3fb593f2f6
Albert <countrik@yandex.ru>
2020-08-23 18:29:09 +0300

    add x-systemd.automount in nfs_client.sh

--- scripts/nfs_client.sh
+++ scripts/nfs_client.sh
@@ -11,7 +11,7 @@ yum install nfs-utils -y
 mkdir /mnt/upload 
 
 # Добавляем строку в fstab для монтирования папки при автозагрузке
-echo "192.168.50.10:/upload /mnt/upload nfs rw,vers=3 0 0" >> /etc/fstab
+echo "192.168.50.10:/upload /mnt/upload nfs rw,vers=3,noauto,x-systemd.automount   0 0" >> /etc/fstab
 
 # Даем права на чтение, запись и выполнение папке upload
 chmod -R 777 /mnt/upload
